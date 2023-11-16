ec60a8f15301fb2fb09161cb8df7efb5d939517a
Yozora0 <pablo.mourato@etudiant.univ-reims.fr>
2023-11-16 11:49:20 +0100

    fix : phpcbf fixing error

--- WR506D/src/State/UserPasswordHasher.php
+++ WR506D/src/State/UserPasswordHasher.php
@@ -1,9 +1,12 @@
 <?php
+
 # api/src/State/UserPasswordHasher.php
 namespace App\State;
+
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 final class UserPasswordHasher implements ProcessorInterface
 {
     public function __construct(private readonly ProcessorInterface $processor, private readonly UserPasswordHasherInterface $passwordHasher)
@@ -14,10 +17,7 @@ final class UserPasswordHasher implements ProcessorInterface
         if (!$data->getPlainPassword()) {
             return $this->processor->process($data, $operation, $uriVariables, $context);
         }
-        $hashedPassword = $this->passwordHasher->hashPassword(
-            $data,
-            $data->getPlainPassword()
-        );
+        $hashedPassword = $this->passwordHasher->hashPassword($data, $data->getPlainPassword());
         $data->setPassword($hashedPassword);
         $data->eraseCredentials();
         return $this->processor->process($data, $operation, $uriVariables, $context);
